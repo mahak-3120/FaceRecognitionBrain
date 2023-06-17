fae4febfcff81ae601a18eb589c90a6294a60b23
mahak-3120 <sainimahak9@gmail.com>
2023-06-17 15:18:41 +0530

    updated packages and added functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
